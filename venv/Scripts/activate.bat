3048241701afa97c0f8572c5fc0d0bde701f09bd
li-chp <752751566@qq.com>
2022-05-20 16:35:13 +0800

    test

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set "VIRTUAL_ENV=D:\Code Repository\netblock\netblock\venv"
+set "VIRTUAL_ENV=D:\Code Repository\netblockProject\netblock\venv"
 
 if defined _OLD_VIRTUAL_PROMPT (
     set "PROMPT=%_OLD_VIRTUAL_PROMPT%"
